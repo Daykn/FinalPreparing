85008b1859cbbaa826960be20dac5b378c0f562b
Daykn <Daykn@users.noreply.github.com>
2017-05-20 10:11:39 +0600

    10:10

--- 2/2/Form1.cs
+++ 2/2/Form1.cs
@@ -16,5 +16,118 @@ namespace _2
         {
             InitializeComponent();
         }
+
+        public static int l=0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n =int.Parse(button1.Text);
+            n += 1;
+            button1.Text = n.ToString();
+            if (n % 2 == 0)
+            {
+                l++;
+                textBox1.Text = l.ToString();
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int n = int.Parse(button2.Text);
+            n += 1;
+            button2.Text = n.ToString();
+            if (n % 2 == 0)
+            {
+                l++;
+                textBox1.Text = l.ToString();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int n = int.Parse(button3.Text);
+            n += 1;
+            button3.Text = n.ToString();
+            if (n % 2 == 0)
+            {
+                l++;
+                textBox1.Text = l.ToString();
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            int n = int.Parse(button6.Text);
+            n += 1;
+            button6.Text = n.ToString();
+            if (n % 2 == 0)
+            {
+                l++;
+                textBox1.Text = l.ToString();
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int n = int.Parse(button5.Text);
+            n += 1;
+            button5.Text = n.ToString();
+            if (n % 2 == 0)
+            {
+                l++;
+                textBox1.Text = l.ToString();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int n = int.Parse(button4.Text);
+            n += 1;
+            button4.Text = n.ToString();
+            if (n % 2 == 0)
+            {
+                l++;
+                textBox1.Text = l.ToString();
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            int n = int.Parse(button9.Text);
+            n += 1;
+            button9.Text = n.ToString();
+            if (n % 2 == 0)
+            {
+                l++;
+                textBox1.Text = l.ToString();
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            int n = int.Parse(button8.Text);
+            n += 1;
+            button8.Text = n.ToString();
+            if (n % 2 == 0)
+            {
+                l++;
+                textBox1.Text = l.ToString();
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            int n = int.Parse(button7.Text);
+            n += 1;
+            button7.Text = n.ToString();
+            if (n % 2 == 0)
+            {
+                l++;
+                textBox1.Text = l.ToString();
+            }
+        }
     }
 }
---
+++ 3/3/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("3")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("881696b9-b22c-41f8-b6f0-019636cf02fa")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
