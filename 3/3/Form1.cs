68a1e44be8759f593779cf233cb0080b732542c1
Daykn <Daykn@users.noreply.github.com>
2017-05-20 10:20:47 +0600

    10:20

--- 3/3/Form1.cs
+++ 3/3/Form1.cs
@@ -16,5 +16,28 @@ namespace _3
         {
             InitializeComponent();
         }
+
+
+        static int n;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            n = 2;
+            if (n == 1)
+            {
+                button1.BackColor = Color.Yellow;
+                n = 2;
+            }
+            if (n == 2)
+            {
+                button1.BackColor = Color.Blue;
+                n = 3;
+            }
+            if (n == 3)
+            {
+                button1.BackColor = Color.Red;
+                n = 1;
+            }
+        }
+        
     }
 }
