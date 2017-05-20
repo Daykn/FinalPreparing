3592b4c94f9421f0e831e20462aa363cb2d00646
Daykn <Daykn@users.noreply.github.com>
2017-05-20 12:02:36 +0600

    all

--- 1.3/1.3/Program.cs
+++ 1.3/1.3/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,18 @@ namespace _1._3
     {
         static void Main(string[] args)
         {
+            DirectoryInfo dir = new DirectoryInfo(@"papka");
+            List<FileInfo> fil = new List<FileInfo>();
+            fil.AddRange(dir.GetFiles());
+            foreach(FileInfo p in fil)
+            {
+                List<int> a = new List<int>();
+                List<int> b = new List<int>();
+                StreamReader sr = new StreamReader(p.FullName);
+                string str = sr.ReadLine();
+                string[] arr = str.Split();
+                 
+            }
         }
     }
 }
