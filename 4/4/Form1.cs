b6cab146dfe1354f0f75386fc2b8bbde2cd5e16e
Daykn <Daykn@users.noreply.github.com>
2017-05-20 10:40:40 +0600

    10:40

--- 4/4/Form1.cs
+++ 4/4/Form1.cs
@@ -12,9 +12,40 @@ namespace _4
 {
     public partial class Form1 : Form
     {
+        public Graphics g;
+        public SolidBrush br;
+        public Bitmap bmp;
+        public Graphics gb;
         public Form1()
         {
             InitializeComponent();
+            g = pictureBox1.CreateGraphics();
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            gb = Graphics.FromImage(bmp);
+            br = new SolidBrush(Color.Purple);
+            timer1.Start();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            gb.FillEllipse(br, e.X,e.Y, 20, 20);
+            pictureBox1.Image = bmp;
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            gb.FillEllipse(br, e.X-10, e.Y-10, 20, 20);
+            pictureBox1.Image = bmp;
         }
     }
 }
