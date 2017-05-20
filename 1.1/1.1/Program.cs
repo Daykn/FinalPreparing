029e5f7e88ea08417480c468c4b519647c5d0a0b
Daykn <Daykn@users.noreply.github.com>
2017-05-20 09:39:08 +0600

    20

--- 1.1/1.1/Program.cs
+++ 1.1/1.1/Program.cs
@@ -8,6 +8,9 @@ namespace _1._1
 {
     class Program
     {
+        public static List<int> a;
+        public static List<int> b;
+        public static bool came = false;
         static void Main(string[] args)
         {
             DirectoryInfo dir = new DirectoryInfo(@"papka");
@@ -15,7 +18,33 @@ namespace _1._1
             fil.AddRange(dir.GetFiles());
             foreach(FileInfo p in fil)
             {
-                Console.WriteLine(p.Name);
+                StreamReader sr = new StreamReader(p.FullName);
+                string str = sr.ReadLine();
+                string[] arr = str.Split();
+                for (int i = 2; i <= int.Parse(arr[0]); i++)
+                {
+                    if (int.Parse(arr[0]) % i == 0)
+                        a.Add(i);
+                }
+                for(int j = 2; j <= int.Parse(arr[1]); j++)
+                {
+                    if (int.Parse(arr[1]) % j == 0)
+                        b.Add(j);
+                }
+                foreach(int z in a)
+                {
+                    for(int u = 0; u <= b.Count; u++)
+                    {
+                        if (z == b[u])
+                        {
+                            came = true;
+                        }
+                        }
+                }
+                if(came)
+                    Console.WriteLine(p.Name + ':' + "yes");
+                    else
+                    Console.WriteLine(p.Name+':'+"no");
             }
 
 
